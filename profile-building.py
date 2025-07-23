1e05d41f8c5b15375bbebd87c6e07cd5f91b57fc
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-07-23 13:02:27 +0300

    Update profile-building.py

--- profile-building.py
+++ profile-building.py
@@ -57,10 +57,12 @@ def profile_generating_tool(user_id):
 
     # نقرأها من جديد للتجربة
     loaded_examples = load_examples_from_json()
-
-    # توليد البروفايل
-    generated_profile = generate_profile_model(data, loaded_examples)
-
-    save_data= insert_generated_profile(user_id,data['organization_name'],generated_profile)
-
-    return JSONResponse(content={"generated_profile": generated_profile})
+    try:
+        # توليد البروفايل
+        generated_profile = generate_profile_model(data, loaded_examples)
+    
+        save_data= insert_generated_profile(user_id,data['organization_name'],generated_profile)
+        return JSONResponse(content={"generated_profile": generated_profile})
+     except Exception as e:
+        # log and return a useful message
+        return JSONResponse(content={"error": str(e)}, status_code=500)
