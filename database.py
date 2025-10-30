153a0b0d341416d4a3594808b9b296d7ffc72042
twaasoulElm3refa <twaslm3rfa@gmail.com>
2025-10-30 11:47:05 +0300

    Update database.py

--- database.py
+++ database.py
@@ -6,11 +6,13 @@ import os
 
 load_dotenv()  # يبحث عن .env في مجلد المشروع الحالي
 
-db_host = os.getenv("db_host")
-db_port = os.getenv("db_port")
-db_user = os.getenv("db_user")
-db_password = os.getenv("db_password")
-db_name = os.getenv("db_name")
+api_key = os.getenv("OPENAI_API_KEY")
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+db_name = os.getenv("DB_NAME")
+
 
 
 def get_db_connection():
@@ -22,7 +24,6 @@ def get_db_connection():
             password=db_password,
             port=db_port
         )
-
         if connection.is_connected():
             print("✅ Connected!")
             return connection
@@ -31,7 +32,7 @@ def get_db_connection():
         print(f"Error connecting to MySQL: {e}")
         return None
 
-def fetch_profile_data(user_id: str ):
+def fetch_press_releases(user_id: str ):
     connection = get_db_connection()
     if connection is None:
         print("Failed to establish database connection")
@@ -41,15 +42,15 @@ def fetch_profile_data(user_id: str ):
         cursor = connection.cursor(dictionary=True)
         query = """
         SELECT * 
-        FROM wpl3_profile_generating_tool
+        FROM wpl3_press_release_Form
         WHERE user_id = %s 
         """
         cursor.execute(query, (user_id,))
 
         # Fetch the first row
-        all_profiles_content = cursor.fetchall()
+        all_user_articles = cursor.fetchall()
 
-        return all_profiles_content
+        return all_user_articles
 
     except Error as e:
         print(f"Error fetching data: {e}")
@@ -61,7 +62,7 @@ def fetch_profile_data(user_id: str ):
 
 
 
-def insert_generated_profile(user_id, organization_name, generated_profile, input_type='Using FORM',request_id=None):
+def update_press_release(user_id, organization_name, article):
     connection = get_db_connection()
     if connection is None:
         return False
@@ -69,11 +70,11 @@ def insert_generated_profile(user_id, organization_name, generated_profile, inpu
     try:
         cursor = connection.cursor()
         query = """
-        INSERT INTO wpl3_profile_result (user_id, organization_name, generated_profile,input_type,request_id)
-        VALUES (%s, %s, %s , %s, %s)
-        ON DUPLICATE KEY UPDATE generated_profile = VALUES(generated_profile)
+        INSERT INTO wpl3_articles (user_id, organization_name, article)
+        VALUES (%s, %s, %s)
+        ON DUPLICATE KEY UPDATE article = VALUES(article)
         """
-        cursor.execute(query, (user_id, organization_name, generated_profile,input_type, request_id))
+        cursor.execute(query, (user_id, organization_name, article))
         connection.commit()
         return True
     except Error as e:
@@ -83,5 +84,3 @@ def insert_generated_profile(user_id, organization_name, generated_profile, inpu
         if connection.is_connected():
             cursor.close()
             connection.close()
-
-
